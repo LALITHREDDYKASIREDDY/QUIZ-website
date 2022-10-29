0504e19dad711ee79917339b98d842f6934dad31
LALITHREDDYKASIREDDY <lalithreddy016@gmail.com>
2022-10-29 13:03:19 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Inline styles are written within the _____ attribute.",
+    answer: "style",
+    options: [
+      
+    "style",
+      "css",
+      "stylesheet",
+      "Both A. and B"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Which property is used to define the background color?",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "bg-color",
+      "background",
+      "background-color"
+    ]
+  },
+    {
+    numb: 3,
+    question: "HTML is the standard ____language for creating Web pages.",
+    answer: "markup",
+    options: [
+      "scripting",
+      "programming",
+      "styling",
+      "markup"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which property is used to define the HTML content to an HTML element with a specific id?",
+    answer: "innerHTML",
+    options: [
+      "innerText",
+      "innerContent",
+      "elementText",
+      "innerHTML"
+    ]
+  },
+    {
+    numb: 5,
+    question: "JavaScript ignores?",
+    answer:"All of the above",
+    options: [
+      "newlines",
+      "tabs",
+      "spaces",
+      "All of the above"
+    ]
+  }
+  
+];
